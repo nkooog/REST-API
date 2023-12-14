b04ca2b1d80513105d29a2ae0a565e9ce86ac22a
jypark <jypark@github.com>
2023-12-14 11:15:26 +0900

    mockbean del

--- src/main/java/com/example/demo/events/EventController.java
+++ src/main/java/com/example/demo/events/EventController.java
@@ -1,11 +1,7 @@
 package com.example.demo.events;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
@@ -37,11 +31,10 @@ public class EventController {
                 .name(eventDto.getName())
                 .build();*/
 
-        // ModelMapper 라이브러리 사용하여 위에 과정생략
         Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
-        URI createUri = linkTo(EventController.class).slash("id").toUri();
-        return ResponseEntity.created(createUri).body(event);
+        URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createUri).body(newEvent);
         // created() 보낸땐 항상 URI 가 필요하다
     }
 
