a62aeb75ae2a4a30ebbb43feb2c9ca33af561735
jypark <jypark@github.com>
2023-12-27 21:21:14 +0900

    Rest Docs sample init

---
+++ src/test/java/com/example/demo/common/RestDocsConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.demo.common;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcBuilderCustomizer() {
+        return configurer -> configurer.operationPreprocessors()
+                .withResponseDefaults(prettyPrint())
+                .withRequestDefaults(prettyPrint());
+        /*
+        return new RestDocsMockMvcConfigurationCustomizer() {
+            @Override
+            public void customize(MockMvcRestDocumentationConfigurer configurer) {
+                configurer.operationPreprocessors().withResponseDefaults(prettyPrint())
+                                                   .withRequestDefaults(prettyPrint());
+            }
+        };
+
+         */
+    }
+
+}
--- src/test/java/com/example/demo/events/EventControllerTest.java
+++ src/test/java/com/example/demo/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.events;
 
+import com.example.demo.common.RestDocsConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.hamcrest.Matchers;
@@ -8,8 +9,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -18,6 +21,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -25,9 +33,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
-@Slf4j
-//@WebMvcTest
+@Import(RestDocsConfiguration.class)
 class EventControllerTest {
 
 	@Autowired
@@ -46,28 +54,74 @@ class EventControllerTest {
 				.beginEnrollmentDateTime(LocalDateTime.of(2023, 10, 18, 21, 28))
 				.closeEnrollmentDateTime(LocalDateTime.of(2023, 11, 18, 21, 28))
 				.beginEventDateTime(LocalDateTime.of(2023, 12, 15, 21, 28))
-				.endEventDateTime(LocalDateTime.of(2023, 12, 16, 21, 28))
-				.basePrice(0)
-				.maxPrice(0)
-				.limitOfEnrollment(100)
-				.location("Inchon City")
-				.build();
+			.endEventDateTime(LocalDateTime.of(2023, 12, 16, 21, 28))
+			.basePrice(0)
+			.maxPrice(0)
+			.limitOfEnrollment(100)
+			.location("Inchon City")
+			.build();
 
-		mockMvc.perform(post("/api/events")
-						.contentType(MediaType.APPLICATION_JSON) // 이 요청 본문에 json을 담아서 보내주고 있다
-						.accept(MediaTypes.HAL_JSON_VALUE) // 원하는 응답
-						.content(objectMapper.writeValueAsString(event)) // Response Body에 작성
-				)
-				.andDo(print())
-				.andExpect(status().isCreated())
-				.andExpect(header().exists(HttpHeaders.LOCATION)) // ??
-				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-				.andExpect(jsonPath("free").exists())
-				.andExpect(jsonPath("offline").exists())
-				.andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)))
-				.andExpect(jsonPath("_links.query-events").exists())
-				.andExpect(jsonPath("_links.self").exists())
-				.andExpect(jsonPath("_links.update-events").exists());
+	mockMvc.perform(post("/api/events")
+					.contentType(MediaType.APPLICATION_JSON) // 이 요청 본문에 json을 담아서 보내주고 있다
+					.accept(MediaTypes.HAL_JSON_VALUE) // 원하는 응답
+					.content(objectMapper.writeValueAsString(event)) // Response Body에 작성
+			)
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(header().exists(HttpHeaders.LOCATION)) // ??
+			.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+			.andExpect(jsonPath("free").exists())
+			.andExpect(jsonPath("offline").exists())
+			.andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)))
+			.andExpect(jsonPath("_links.query-events").exists())
+			.andExpect(jsonPath("_links.self").exists())
+			.andExpect(jsonPath("_links.update-events").exists())
+			.andDo(document("create-event"
+			,	links(
+						 linkWithRel("self").description("link to self")
+						,linkWithRel("query-events").description("link to query")
+						,linkWithRel("update-events").description("link to update")
+					),
+					requestHeaders(
+						 headerWithName(HttpHeaders.ACCEPT).description("accept header")
+						,headerWithName(HttpHeaders.CONTENT_TYPE).description("content-type header")
+					),
+					requestFields(
+						 fieldWithPath("name").description("Name of new event")
+						,fieldWithPath("description").description("description of new event")
+						,fieldWithPath("beginEnrollmentDateTime").description("beginEnrollmentDateTime of new event")
+						,fieldWithPath("closeEnrollmentDateTime").description("closeEnrollmentDateTime of new event")
+						,fieldWithPath("beginEventDateTime").description("beginEventDateTime of new event")
+						,fieldWithPath("endEventDateTime").description("endEventDateTime of new event")
+						,fieldWithPath("basePrice").description("basePrice of new event")
+						,fieldWithPath("maxPrice").description("maxPrice of new event")
+						,fieldWithPath("limitOfEnrollment").description("limitOfEnrollment of new event")
+						,fieldWithPath("location").description("location of new event")
+					),
+					responseHeaders(
+						  headerWithName(HttpHeaders.LOCATION).description("Location header")
+						 ,headerWithName(HttpHeaders.CONTENT_TYPE).description("Content-type")
+					),
+					responseFields(
+						 fieldWithPath("id").description("id of new event")
+						,fieldWithPath("name").description("Name of new event")
+						,fieldWithPath("description").description("description of new event")
+						,fieldWithPath("beginEnrollmentDateTime").description("beginEnrollmentDateTime of new event")
+						,fieldWithPath("closeEnrollmentDateTime").description("closeEnrollmentDateTime of new event")
+						,fieldWithPath("beginEventDateTime").description("beginEventDateTime of new event")
+						,fieldWithPath("endEventDateTime").description("endEventDateTime of new event")
+						,fieldWithPath("basePrice").description("basePrice of new event")
+						,fieldWithPath("maxPrice").description("maxPrice of new event")
+						,fieldWithPath("limitOfEnrollment").description("limitOfEnrollment of new event")
+						,fieldWithPath("location").description("location of new event")
+						,fieldWithPath("free").description("it tells if this event is free")
+						,fieldWithPath("offline").description("it tells if this event is offline")
+						,fieldWithPath("eventStatus").description("event status")
+						,fieldWithPath("_links.self.href").description("links.self")
+						,fieldWithPath("_links.query-events.href").description("links.query-events")
+						,fieldWithPath("_links.update-events.href").description("links.update-events")
+					)
+			));
 	}
 
 	@Test
